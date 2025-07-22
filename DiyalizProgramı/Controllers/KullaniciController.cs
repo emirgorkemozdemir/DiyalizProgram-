36073daf2d64b6162de67b252ceba2bc3e771dca
EMİR <emirgorkemozdemir@gmail.com>
2025-07-22 17:52:46 +0300

    güncellendi

--- DiyalizProgramı/Controllers/KullaniciController.cs
+++ DiyalizProgramı/Controllers/KullaniciController.cs
@@ -1,5 +1,6 @@
 ﻿
 using BussinessLayer;
+using DiyalizProgramı.Extra;
 using EntityLayer;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,8 +19,22 @@ namespace DiyalizProgramı.Controllers
         [HttpPost]
         public async Task<IActionResult> Giris(Kullanici user)
         {
-            var tum_kullanicilar = await kullanici_islemleri.Methods.List();
-            var secili_kullanici = tum_kullanicilar.Where(m => m.KullaniciAdi == user.KullaniciAdi && m.SifreHash == user.SifreHash).SingleOrDefault();
+            if(user.KullaniciAdi == null)
+            {
+                ViewBag.msg = "Kullanıcı adı boş olamaz";
+                return View();
+            }
+            else if (user.SifreHash == null)
+            {
+                ViewBag.msg = "Şifre boş olamaz";
+                return View();
+            }
+                var tum_kullanicilar = await kullanici_islemleri.Methods.List();
+
+            // kullanıcı şifresini sha256 ile hashle
+            string sha_sifre = SifreSha.ComputeSha256Hash(user.SifreHash);
+
+            var secili_kullanici = tum_kullanicilar.Where(m => m.KullaniciAdi == user.KullaniciAdi && m.SifreHash == sha_sifre).SingleOrDefault();
             if (secili_kullanici != null)
             {
                 // giriş başarılı
